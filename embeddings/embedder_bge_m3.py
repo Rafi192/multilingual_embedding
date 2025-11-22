051984e51b290f69df99adc288635eac9d17d94c
Rafi192 <rafihasanmmsc@gmail.com>
2025-11-22 16:38:31 +0600

    Commif of bge-m3 embedding

---
+++ embeddings/embedder_bge_m3.py
@@ -0,0 +1,14 @@
+from sentence_transformers import SentenceTransformer
+import numpy as np
+
+
+class BGE_M3_Embedder:
+    def __init__(self):
+        self.model = SentenceTransformer("BAAI/bge-m3")
+
+
+    def embed(self, texts):
+        if isinstance(texts, str):
+            texts = [texts]
+            vectors = self.model.encode(texts, normalize_embeddings=True)
+            return np.array(vectors).tolist()
